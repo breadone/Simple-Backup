83ac9330fb858e7bd06efe86df8f7bb5bdc1e32d
PradyunS <pradyun.setti@gmail.com>
2020-05-18 07:04:52 +1200

    attempt md5

--- src/backup/backup.java
+++ src/backup/backup.java
@@ -1,13 +1,47 @@
 package backup;
 
+import java.io.*;
+import java.security.MessageDigest;
+
 public class backup {
 
     public static void main(String[] args) {
         StartService();
     }
 
+    public static String QueryPath() {
+        String path;
+        System.out.print("paste the path to the file or folder to backup: ");
+        path = System.console().readLine();
+        return path;
+    }
+
     public static void StartService() {
-    
+        String path = QueryPath();
+        //byte[] md5 = new byte[1024];
+        long md5;
+        //generate md5
+        md5 = (long) CreateChecksum(path);
+
+
+
+    }
+
+
+    public static byte[] CreateChecksum(String filename) throws Exception {
+        InputStream fis =  new FileInputStream(filename);
+
+        byte[] buffer = new byte[1024];
+        MessageDigest complete = MessageDigest.getInstance("MD5");
+        int numRead;
+        do {
+            numRead = fis.read(buffer);
+            if (numRead > 0) {
+                complete.update(buffer, 0, numRead);
+             }
+         } while (numRead != -1);
+            fis.close();
+            return complete.digest();
     }
 
 }

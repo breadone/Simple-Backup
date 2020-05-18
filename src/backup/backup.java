4f5d165d26baf5a7f8ecceae05d3e1c81d3aba1b
pradyun laptop <pradyun.setti@gmail.com>
2020-05-18 15:56:28 +1200

    try to return int on genhash

--- src/backup/backup.java
+++ src/backup/backup.java
@@ -3,6 +3,8 @@ package backup;
 import java.io.*;
 import java.security.MessageDigest;
 
+import sun.security.provider.MD5;
+
 public class backup {
 
     public static void main(String[] args) {
@@ -21,15 +23,15 @@ public class backup {
         //byte[] md5 = new byte[1024];
         long md5;
         //generate md5
-        md5 = (long) CreateChecksum(path);
-
-
+        md5 = CreateChecksum(path);
+        System.out.println(md5);
 
     }
 
 
-    public static byte[] CreateChecksum(String filename) throws Exception {
+    public static long CreateChecksum(String filename) throws Exception {
         InputStream fis =  new FileInputStream(filename);
+        long hash = 0;
 
         byte[] buffer = new byte[1024];
         MessageDigest complete = MessageDigest.getInstance("MD5");
@@ -41,7 +43,13 @@ public class backup {
              }
          } while (numRead != -1);
             fis.close();
-            return complete.digest();
+            //return complete.digest();
+
+            for (int i = 0; i < complete.digest().length - 1; i++) {
+                hash += buffer[i];
+            }
+
+            return hash;
     }
 
 }

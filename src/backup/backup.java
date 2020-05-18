594c45a0e4efefce8a3f6c7382c2863a0c475dc5
PradyunS <pradyun.setti@gmail.com>
2020-05-18 21:32:31 +1200

    FINALLY got hash working, back to md5 hah

--- src/backup/backup.java
+++ src/backup/backup.java
@@ -3,10 +3,12 @@ package backup;
 import java.io.*;
 import java.util.zip.CRC32;
 import java.util.zip.CheckedInputStream;
+import java.lang.Object;
+import java.security.MessageDigest;
 
 public class backup {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         StartService();
     }
 
@@ -17,12 +19,12 @@ public class backup {
         return path;
     }
 
-    public static void StartService() {
-        String path = QueryPath();
-        long md5;
+    public static void StartService() throws IOException {
+        File path = new File(QueryPath());
+        byte[] hash = new byte[65535];
         //generate md5
-        md5 = GenerateChecksum(path);
-        System.out.println(md5);
+        hash = bGen.Hash.MD5.checksum(path);
+        System.out.println(hash);
 
         
 
@@ -53,12 +55,46 @@ public class backup {
     }*/
 
 
-    public static long GenerateChecksum(String path) throws IOException {
+   /* public static long GenerateChecksum(String path) throws IOException {
+
         InputStream stream = new FileInputStream(path);
         CheckedInputStream CInputStream = new CheckedInputStream(stream, new CRC32());
         byte[] buffer = new byte[65536];
         
+        long hash = 
 
         return 1238;
-    }
-}
+    } */
+
+  /*  private static String getFileChecksum(/*MessageDigest digest, File file) throws IOException {
+        //Get file input stream for reading the file content
+        FileInputStream fis = new FileInputStream(file);
+        MessageDigest digest = new MessageDigest("SHA-256");
+
+        //Create byte array to read data in chunks
+        byte[] byteArray = new byte[1024];
+        int bytesCount = 0; 
+        
+        //Read file data and update in message digest
+        while ((bytesCount = fis.read(byteArray)) != -1) {
+            digest.update(byteArray, 0, bytesCount);
+        };
+        
+        //close the stream; We don't need it now.
+        fis.close();
+        
+        //Get the hash's bytes
+        byte[] bytes = digest.digest();
+        
+        //This bytes[] has bytes in decimal format;
+        //Convert it to hexadecimal format
+        StringBuilder sb = new StringBuilder();
+        for(int i=0; i< bytes.length ;i++)
+        {
+            sb.append(Integer.toString((bytes[i] & 0xff) + 0x100, 16).substring(1));
+    } 
+     
+    //return complete hash
+   return sb.toString(); */
+} 
+
